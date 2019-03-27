0cf9bcb63ba942140f83022584d2eea38517c597
Mark Rudolph <44739691+mrudolph1229@users.noreply.github.com>
2019-03-26 22:05:50 -0700

    First

---
+++ StarterCode/assets/app.js
@@ -0,0 +1,51 @@
+// @TODO: YOUR CODE HERE!
+function makeResponsive() {
+
+    // if the SVG area isn't empty when the browser loads,
+    // remove it and replace it with a resized version of the chart
+    var svgArea = d3.select("#scatter").select("svg");
+  
+    // clear svg is not empty
+    if (!svgArea.empty()) {
+      svgArea.remove();
+    }
+  
+    // SVG wrapper dimensions are determined by the current width and
+    // height of the browser window.
+    var svgWidth = window.innerWidth;
+    var svgHeight = window.innerHeight;
+  
+    var margin = {
+      top: 50,
+      bottom: 50,
+      right: 50,
+      left: 50
+    };
+  
+    var height = svgHeight - margin.top - margin.bottom;
+    var width = svgWidth - margin.left - margin.right;
+
+    // Append SVG element
+    var svg = d3
+    .select(".chart")
+    .append("svg")
+    .attr("height", svgHeight)
+    .attr("width", svgWidth);
+
+    // Append group element
+    var chartGroup = svg.append("g")
+        .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+    d3.csv("./data/data.csv", function(error, healthData) {
+        if (error) return console.warn(error);
+
+        console.log(healthData)
+    })    
+
+};
+
+// When the browser loads, makeResponsive() is called.
+makeResponsive();
+
+// When the browser window is resized, makeResponsive() is called.
+d3.select(window).on("resize", makeResponsive);
