edadf9ab2ba3be1a5abd93eaea7d3c92b3f9097f
Mark Rudolph <44739691+mrudolph1229@users.noreply.github.com>
2019-03-29 21:51:48 -0700

    Third

--- StarterCode/assets/app.js
+++ StarterCode/assets/app.js
@@ -1,4 +1,9 @@
 // @TODO: YOUR CODE HERE!
+
+// d3.select(window).on("resize", makeResponsive);
+
+// loadChart();
+
 function makeResponsive() {
 
     // if the SVG area isn't empty when the browser loads,
@@ -8,8 +13,11 @@ function makeResponsive() {
     // clear svg is not empty
     if (!svgArea.empty()) {
       svgArea.remove();
+      // loadChart()
     }
+// }  
   
+// function loadChart() {
     // SVG wrapper dimensions are determined by the current width and
     // height of the browser window.
     var svgWidth = window.innerWidth;
@@ -27,7 +35,7 @@ function makeResponsive() {
 
     // Append SVG element
     var svg = d3
-    .select(".chart")
+    .select("#scatter")
     .append("svg")
     .attr("height", svgHeight)
     .attr("width", svgWidth);
@@ -36,10 +44,65 @@ function makeResponsive() {
     var chartGroup = svg.append("g")
         .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-    d3.csv("./data/data.csv", function(error, healthData) {
+    d3.csv("./assets/data.csv", function(error, healthData) {
         if (error) return console.warn(error);
 
         console.log(healthData)
+
+        healthData.forEach(function(data) {
+          data.smokes = +data.smokes;
+          data.age = +data.age;
+        });
+
+        // create scales
+        var xLinearScale = d3.scaleLinear()
+          .domain(d3.extent(healthData, d => d.age))
+          .range([0, width]);
+
+        var yLinearScale = d3.scaleLinear()
+          .domain([0, d3.max(healthData, d => d.smokers)])
+          .range([height, 0]);
+
+        // create axes
+        var xAxis = d3.axisBottom(xLinearScale);
+        var yAxis = d3.axisLeft(yLinearScale).ticks(6);
+
+        // append axes
+        chartGroup.append("g")
+          .attr("transform", `translate(0, ${height})`)
+          .call(xAxis);
+
+        chartGroup.append("g")
+          .call(yAxis);
+
+        chartGroup.selectAll("circle")
+        .data(healthData)
+        .enter()
+        .append("circle")
+        .attr("cx", d => xLinearScale(d.age))
+        .attr("cy", d => yLinearScale(d.smokers))
+        .attr("r", "10")
+        .attr("fill", "blue")
+        .attr("stroke-width", "1")
+        .attr("stroke", "black")
+        .attr("opacity", ".8");  
+
+        chartGroup.selectAll("text")
+        .data(healthData)
+        .enter()
+        .append("text")
+        .text(d => d.abbr)
+        .attr("x", d => xLinearScale(d.age))
+        .attr("y", d => yLinearScale(d.smokers))
+        .attr("text-anchor", "middle")
+        .attr("alignment-baseline", "central")
+        .attr("font_family", "sans-serif")
+        .attr("font-size", "11px")
+        .attr("fill", "white")
+        .style("font-weight", "bold");
+
+        // console.log(d.age)
+        // console.log(d.smokes)
     })    
 
 };
